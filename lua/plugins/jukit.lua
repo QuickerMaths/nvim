ef64de0beef50eb6fa7bb1d53841f7f6ec8564ba
QuickerMaths <mateusz.szalowicz@gmail.com>
2025-05-13 22:26:22 +0200

    updated jukit to support kitty

--- lua/plugins/jukit.lua
+++ lua/plugins/jukit.lua
@@ -2,6 +2,9 @@ return {
 	"luk400/vim-jukit",
 	lazy = false,
 	config = function()
+		vim.g.jukit_terminal = 'kitty'
 		vim.g.jukit_pdf_viewer = 'tdf'
+		vim.g.jukit_output_new_os_window = 1
+		vim.g.jukit_inline_plotting = 1
 	end
 }
